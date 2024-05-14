bf14055b1f828cc9bdfa368ddeca2822d4a1c386
Booms007 <33290615+Booms007@users.noreply.github.com>
2024-05-14 02:28:50 -0700

    Add files via upload

---
+++ src/navigation.js
@@ -0,0 +1,54 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: `Home`,
+      href: getPermalink("/"),
+    },
+    {
+      text: 'CFP',
+      href: getPermalink("/cfp"),
+    },
+    {
+      text: 'Sponsors',
+      href: getPermalink("/sponsors"),
+    },
+    {
+      text: 'Contact',
+      href: getPermalink("/contact"),
+    },
+    // {
+    //   text: 'Team',
+    //   href: getPermalink("/team"),
+    // }
+  ],
+  actions: [],
+};
+
+export const footerData = {
+  links: [
+    // {
+    //   title: 'Company',
+    //   links: [
+    //     { text: 'About', href: '#' },
+    //     { text: 'Blog', href: '#' },
+    //     { text: 'Careers', href: '#' },
+    //     { text: 'Press', href: '#' },
+    //     { text: 'Inclusion', href: '#' },
+    //     { text: 'Social Impact', href: '#' },
+    //     { text: 'Shop', href: '#' },
+    //   ],
+    // },
+  ],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') }
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/bsideschennai' },
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: "https://www.linkedin.com/groups/14357686/" }
+  ],
+  footNote: `
+  Powered by <b>Cloudflare Technologies</b> and <b>Astro</b>
+  `,
+};
